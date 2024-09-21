3f964cfb93756251331b753da56de4732974f1c2
Saravanan2004saro <167509963+Saravanan2004saro@users.noreply.github.com>
2024-09-21 09:33:58 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
         <section id="contact">
             <h2>Contact Me</h2>
             <p>Email: Saravanan3625vkl@gmail.com</p>
-            <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile">My LinkedIn</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/saravana-kumar-518a7b255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">My LinkedIn</a></p>
             <p>Phone: 9842151157</p>
         </section>
     );
